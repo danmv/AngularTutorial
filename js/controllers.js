8b1307426354e1953e5a928fddd563b58eff6a13
Daniel MV <dmvilla05@gmail.com>
2016-04-20 17:25:22 -0700

    when through almost full tutorial and create new files.

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,33 @@
-/**
- * Created by chq-danielma on 4/19/2016.
- */
-var myApp = angular.module('myApp', []);
+// /**
+//  * Created by chq-danielma on 4/19/2016.
+//  */
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
     $http.get('js/data.json').success(function (data) {
         $scope.artists = data;
         $scope.artistOrder = 'name';
+    });
+}]);
+
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $http.get('js/data.json').success(function (data) {
+        $scope.artists = data;
+        $scope.whichItem = $routeParams.itemId;
+
+        if ($routeParams.itemId > 0) {
+            $scope.prevItem = Number($routeParams.itemId) - 1;
+        } else {
+            $scope.prevItem = $scope.artists.length-1;
+        }
+
+
+        if ($routeParams.itemId < $scope.artists.length-1) {
+            $scope.nextItem = Number($routeParams.itemId) + 1;
+        } else {
+            $scope.nextItem = 0;
+        }
+
     });
 }]);
